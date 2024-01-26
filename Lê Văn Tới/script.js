f374def06c6ac85873a2457dd1207b96e1c0a4a5
letoi7831 <letoi7831@gmail.com>
2024-01-26 20:49:30 +0700

    commit 7

--- Lê Văn Tới/script.js
+++
@@ -1,9 +0,0 @@
-const adressbtn = document.querySelector('#adress-form')   /* khai báo */
-const adressclose = document.querySelector('#adress-close')
-console.log(adressclose)
-adressbtn.addEventListener("click", function(){          /* thêm sự kiện , thê sự kiẹn là khi click vào thì nó thực hiện hoạt động function */
-    document.querySelector('.adress-form').style.display = "flex" /*khi click vào thì nó hiện ra cái adress-form */
-})
-adressclose.addEventListener("click", function(){
-    document.querySelector('.adress-form').style.display = "none" /*khi click vào thì nó mất đi cái adress-form */
-})

3c88dc16b44b382b66b8b0d7f71d1ccd6fca562f
letoi7831 <letoi7831@gmail.com>
2024-02-24 21:48:03 +0700

    commit 10

---
+++ Lê Văn Tới/main.js
@@ -0,0 +1,14 @@
+// Menu
+const overlay = document.querySelector("#overlay");
+
+menuButton.addEventListener("click", () => {
+  menu.classList.toggle("navbar__open");
+  menuButton.classList.toggle("open");
+  overlay.classList.toggle("show");
+});
+
+overlay.addEventListener("click", () => {
+  menu.classList.toggle("navbar__open");
+  menuButton.classList.toggle("open");
+  overlay.classList.toggle("show");
+});
